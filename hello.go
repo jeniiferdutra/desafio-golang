5612bb7fd0b14b1e26a53199679df5d68535fddb
jenifer rocha <jeniferdutra2000@icloud.com>
2026-06-22 22:24:56 -0300

    setup: inicio do desafio em golang

--- hello.go
+++ hello.go
@@ -11,7 +11,7 @@ func main() {
 
 	switch comando {
 	case 1: 
-		fmt.Println("Monitorando...")
+		iniciarMonitoramento()
 	case 2:
 		fmt.Println("Exibindo Logs...")
 	case 0:
@@ -42,4 +42,8 @@ func exibeMenu() {
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do programa")
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
 }
